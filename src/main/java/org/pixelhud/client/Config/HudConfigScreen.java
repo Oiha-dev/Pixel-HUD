1dcee5ff74f0459c717aa489d0624985a12a4570
Oiha-dev <cenes.gauthier@gmail.com>
2024-01-16 00:24:06 +0100

    Update and creation of the config file

--- src/main/java/org/pixelhud/client/Config/HudConfigScreen.java
+++ src/main/java/org/pixelhud/client/Config/HudConfigScreen.java
@@ -7,24 +7,29 @@ import me.shedaniel.clothconfig2.api.ConfigScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
-public class HudConfigScreen{
 
-    static int a = 1;
+public class HudConfigScreen {
+
+    private static int xCoordinate = 1;
+    private static int yCoordinate = 1;
+
     public static Screen getConfigScreen(Screen parentScreen) {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setParentScreen(parentScreen)
-                .setDefaultBackgroundTexture(new Identifier("minecraft:textures/block/bee_nest_side.png"))
+                .setDefaultBackgroundTexture(new Identifier("minecraft:textures/block/oak_planks.png"))
                 .setTitle(new TranslatableText("config.PixelHud.title"));
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-        ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("config.PixelHud.coordinate"));
-        general.addEntry(entryBuilder.startIntField(new TranslatableText("config.PixelHud.coordinate.x"), currentValue)
+        ConfigCategory coordinate = builder.getOrCreateCategory(new TranslatableText("config.PixelHud.coordinate"));
+
+        coordinate.addEntry(entryBuilder.startIntSlider(new TranslatableText("config.PixelHud.coordinate.x"), xCoordinate)
                 .setDefaultValue(1)
-                .setSaveConsumer(x_coo -> currentValue = x_coo)
+                .setSaveConsumer(x_coo -> xCoordinate = x_coo)
                 .build());
-        general.addEntry(entryBuilder.startIntField(new TranslatableText("config.PixelHud.coordinate.y"), currentValue)
+
+        coordinate.addEntry(entryBuilder.startIntField(new TranslatableText("config.PixelHud.coordinate.y"), yCoordinate)
                 .setDefaultValue(1)
-                .setSaveConsumer(y_coo -> currentValue = y_coo)
+                .setSaveConsumer(y_coo -> yCoordinate = y_coo)
                 .build());
 
 
--- src/main/java/org/pixelhud/client/Pixel_HUDClient.java
+++ src/main/java/org/pixelhud/client/Pixel_HUDClient.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -17,28 +18,11 @@ public class Pixel_HUDClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HudRenderCallback.EVENT.register(new HudOverlay());
-
-        // Register the command
-        registerCommand();
-    }
-
-    private void registerCommand() {
-        CommandDispatcher<FabricClientCommandSource> dispatcher = ClientCommandManager.DISPATCHER;
-
-        dispatcher.register(
-                LiteralArgumentBuilder
-                        .<FabricClientCommandSource>literal("hudconfig")
-                        .executes(context -> {
-                            // Get the player who executed the command
-                            ClientPlayerEntity player = MinecraftClient.getInstance().player;
-
-                            // Check if the player is in-game
-                            if (player != null) {
-                                MinecraftClient.getInstance().setScreen(HudConfigScreen.getConfigScreen(MinecraftClient.getInstance().currentScreen));
-                            }
-
-                            return 1;
-                        })
-        );
+        Pixel_HudKeyBinding.register();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (Pixel_HudKeyBinding.getCalculatorKeyBinding().isPressed()) {
+                MinecraftClient.getInstance().setScreen(HudConfigScreen.getConfigScreen(MinecraftClient.getInstance().currentScreen));
+            }
+        });
     }
 }
